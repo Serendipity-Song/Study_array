ad8c7f97b393975f8322f8cca11cf5fb5fa3099b
Serendipity-Song <thdalwh3867@gmail.com>
2021-09-12 23:56:25 +0900

    자료구조 삽입정렬

---
+++ 자료구조스터디-정렬/소스.cpp
@@ -0,0 +1,28 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int array[] = { 7,5,9,0,3,1 };
+	int len = sizeof(array) / sizeof(array[0]); //전체 배열 바이트/요소바이트
+
+	int temp;
+
+	for (int i = 1; i < len; i++) {
+		for (int j = i; j > 0; j--) {
+			if (array[j] < array[j - 1]) {
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+
+			}
+			else break;
+		}
+	}
+
+	for (int i = 0; i < len; i++) {
+		cout << array[i] << " ";
+	}
+}
